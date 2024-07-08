a27be5e1db5833ed57cdd054a63392c70ad6ef55
Azgad García Zúñiga <amerced@cibahealth.com>
2024-07-07 22:57:10 -0600

    Adding kubernetes provider

--- extra-providers.tf
+++ extra-providers.tf
@@ -23,3 +23,14 @@ provider "kubectl" {
   #   args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
   # }
 }
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  config_path            = "~/.kube/config"
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1"
+  #   args        = ["eks", "get-token", "--cluster-name", var.cluster_name]
+  #   command     = "aws"
+  # }
+}
